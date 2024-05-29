881d18ad53a863cf31d3291f251f7239c599ccf7
lopez marselo <lopezmarselo16@gmail.com>
2024-05-29 09:50:06 +0700

    Add project files.

---
+++ PRESENSI  PBO LOPEZ MARSELO/Program.cs
@@ -0,0 +1,8 @@
+﻿class program
+{
+    public static void main(string[] args)
+    {
+        Console.WriteLine("Absensi Video Github");
+        Console.WriteLine("Lopez Marselo | 232410103033")
+    }
+}
